65857a87252956299db6685e5ba4df3e661f1fdc
yigitcukuren <yigitnerukuc@gmail.com>
2016-03-05 22:56:47 +0200

    Getting data from database to render a view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Yazi
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    yazilar = Yazi.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
+    return render(request, 'blog/post_list.html', {'yazilar': yazilar})

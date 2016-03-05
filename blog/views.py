385e118f4a5dac3e203d5eddba5602848be12266
yigitcukuren <yigitnerukuc@gmail.com>
2016-03-05 23:20:26 +0200

    Detailed post page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Yazi
 # Create your views here.
@@ -6,3 +6,7 @@ from .models import Yazi
 def post_list(request):
     yazilar = Yazi.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
     return render(request, 'blog/post_list.html', {'yazilar': yazilar})
+
+def post_detail(request,pk):
+    yazi = get_object_or_404(Yazi, pk=pk)
+    return render(request, 'blog/post_detail.html', {'yazi': yazi})
